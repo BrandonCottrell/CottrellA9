344e7b02b389c7184d4e5add5177eb2dfa9b35d5
Brandon Cottrell <bcottrell@my.wctc.edu>
2023-04-09 17:11:34 -0500

    A5 commit

---
+++ Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using ApplicationTemplate.Models;
+
+namespace ApplicationTemplate.Data
+{
+    internal class Context
+    {
+       
+    }
+}
--- Services/MainService.cs
+++ Services/MainService.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using ApplicationTemplate.Data;
+using ApplicationTemplate.Models;
+using Newtonsoft.Json;
+using static System.Net.WebRequestMethods;
 
 namespace ApplicationTemplate.Services;
 
@@ -19,23 +29,50 @@ public class MainService : IMainService
         string choice;
         do
         {
-            Console.WriteLine("1) Add Movie");
-            Console.WriteLine("2) Display All Movies");
-            Console.WriteLine("X) Quit");
-            choice = Console.ReadLine();
+            List<UInt64> MovieIds = new List<UInt64>();
+            List<Movie> MoviesList = new List<Movie>();
 
-            // Logic would need to exist to validate inputs and data prior to writing to the file
-            // You would need to decide where this logic would reside.
-            // Is it part of the FileService or some other service?
+            // display choices to user
+            Console.WriteLine("1) Add Movie");
+            Console.WriteLine("2) View All Movies");
+            Console.WriteLine("Enter to quit");
+
+            // input selection
+            choice = Console.ReadLine();
+            Console.WriteLine($"User Choice: {choice}");
+
             if (choice == "1")
             {
-                _fileService.Write();
+                var sw = new StreamWriter("movies.json");
+    
+                var movie = new Movie();
+                Console.WriteLine("Enter Movie Title");
+                movie.Title = Console.ReadLine();
+                Console.WriteLine("Enter Genre");
+                movie.Genre = Console.ReadLine();
+                movie.Id = MovieIds.Count + 1;
+
+
+                var json = JsonConvert.SerializeObject(movie);
+                sw.WriteLine(json);
+                sw.Close();
+
             }
             else if (choice == "2")
             {
-                _fileService.Read();
+                var sr = new StreamReader("movies.json");
+                var line = sr.ReadLine();
+
+                Movie m = JsonConvert.DeserializeObject<Movie>(line);
+
+
+
+                Console.WriteLine($"My movie is: {m.Id}, {m.Title}, {m.Genre}");
+                sr.Close();
             }
-        }
-        while (choice != "X");
+        } while (choice == "1" || choice == "2");
     }
 }
+
+
+
